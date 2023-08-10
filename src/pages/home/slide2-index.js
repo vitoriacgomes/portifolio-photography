7b71e570e113b4828b7f3c7a9a1528f8c620d305
vitoria <vi21cgomes@gmail.com>
2023-08-09 21:11:01 -0300

    atualização da responsividade

--- src/pages/home/slide2-index.js
+++ src/pages/home/slide2-index.js
@@ -6,7 +6,7 @@ const Slide2 = ({onPessoasButtonClick}) => {
         <section class="home-container-2">
             <div class="container px-5 py-24 mx-auto">
                 <div class="flex flex-col text-center w-full mb-12">
-                    <h1 class="home-title sm:text-3xl mb-4 ">PESSOAS</h1>
+                    <h1 class="home-title sm:text-3xl mb-4">PESSOAS</h1>
                     <button class="button-home pessoas flex mx-auto border p-3 px-6" onClick={onPessoasButtonClick}>View</button>
                 </div>
             </div>
--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -4,13 +4,13 @@ const Menu = ({onLugaresButtonClick, onHomeButtonClick, onPessoasButtonClick, on
   return (
     <div className='menu'>
         <header class="menu-container text-600 body-font">
-            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <div class="container-menu mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <nav class="nav md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                    <button class="mr-5 hover:text-900" onClick={onHomeButtonClick}>Home</button>
-                    <button class="mr-5 hover:text-900" onClick={onPessoasButtonClick}>Pessoas</button>
-                    <button class="mr-5 hover:text-900" onClick={onLugaresButtonClick}>Lugares</button>
-                    <button class="mr-5 hover:text-900" onClick={onSobreButtonClick}>Sobre</button>
-                    <button class="mr-5 hover:text-900" onClick={onContatoButtonClick}>Contato</button>
+                    <button class="nav-me mr-5 hover:text-900" onClick={onHomeButtonClick}>Home</button>
+                    <button class="nav-me mr-5 hover:text-900" onClick={onPessoasButtonClick}>Pessoas</button>
+                    <button class="nav-me mr-5 hover:text-900" onClick={onLugaresButtonClick}>Lugares</button>
+                    <button class="nav-me mr-5 hover:text-900" onClick={onSobreButtonClick}>Sobre</button>
+                    <button class="nav-me mr-5 hover:text-900" onClick={onContatoButtonClick}>Contato</button>
                 </nav>
             </div>
         </header>
